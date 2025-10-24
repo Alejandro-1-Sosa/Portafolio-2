f48f246779993e7acb3e1ab4e0840dacf99dbfa1
Alejandro-1-Sosa <141190092+Alejandro-1-Sosa@users.noreply.github.com>
2025-10-24 15:05:06 -0300

    commit

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-let menuVisible = false;
-// Función que oculta o muestra el menu
 function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList="";
-        menuVisible = false;
-    }else{
-        document.getElementById("nav").classList = "responsive";
-        menuVisible = true;
-    }
-}
-
-function seleccionar(){
-    // Oculto el menu una vez que selecciono una opción
-    document.getElementById("nav").classList = "";
-    menuVisible = false;
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("responsive");
 }
